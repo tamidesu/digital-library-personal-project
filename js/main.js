19bf9b09b251868b2bf6cd28faae9387d6631d5b
Temirlan A <gankstargamer@gmail.com>
2025-10-15 18:52:14 +0500

    products changed style

--- js/main.js
+++ js/main.js
@@ -9,4 +9,12 @@ document.addEventListener("DOMContentLoaded", () => {
       alert(`You have added "${title}" to your cart (${price})`);
     });
   });
+
+  document.querySelectorAll('.product').forEach(card => {
+  card.addEventListener('mousemove', e => {
+    const rect = card.getBoundingClientRect();
+    card.style.setProperty('--x', `${e.clientX - rect.left}px`);
+    card.style.setProperty('--y', `${e.clientY - rect.top}px`);
+  });
+});
 });
